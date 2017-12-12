c28ca972896c44d54e7a93d4a24bba85198517e1
cherrymaryma <30802619+cherrymaryma@users.noreply.github.com>
2017-12-12 14:31:39 +0800

    Update Extract_Seqs_using_id.py

--- Extract_Seqs_using_id.py
+++ Extract_Seqs_using_id.py
@@ -1,4 +1,6 @@
-###Author:Ju Feng, contact:Richieju520@gmail.com
+###Author:Ju Feng, Environmental Biotechnology Lab, The University of Hong Kong
+###Contact:Richieju520@gmail.com
+###This script is a draft and the author is not responsible for its misuse.
 
 file_name1=raw_input("Enter the full name of id.txt: ")
 file_name2=raw_input("Enter the full name of the database fasta: ") 
